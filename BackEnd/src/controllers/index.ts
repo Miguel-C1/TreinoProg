c156308821d694ce6669fb68df26b4b76c0c6e22
Miguel Carvalho <doxito007@gmail.com>
2024-06-18 18:47:40 -0300

    A

--- BackEnd/src/controllers/index.ts
+++ BackEnd/src/controllers/index.ts
@@ -2,3 +2,5 @@ export { default as ControllerGroups } from './ControllerGroups';
 export { default as ControllerUsers } from './ControllerUser';
 export { default as ControllerExercises } from './ControllerExercise';
 export { default as ControllerTraining } from './ControllerTraining';
+export { default as ControllerAcompanhamento } from './ControllerAcompanhamento';
+export { default as ControllerImages } from './ControllerImages';

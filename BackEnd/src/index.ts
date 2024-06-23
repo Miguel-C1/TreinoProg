e8127ebc151f1ead4fff0f823cf57eac126a5e23
Miguel Carvalho <doxito007@gmail.com>
2024-06-23 18:26:27 -0300

    Ajustando Tamanho de Imagem

--- BackEnd/src/index.ts
+++ BackEnd/src/index.ts
@@ -43,7 +43,7 @@ const upload = multer({ storage: storage });
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: '10mb' })); // Adjust '10mb' as per your requirements
 app.use(cors());
 app.use(router);
 
